edd3748de8c9246a753d86313c8700fc255d9c63
yanesko <yanesko87@gmail.com>
2019-01-30 09:10:37 +1300

    Emit newMessage on createMessage:

--- public/js/index.js
+++ public/js/index.js
@@ -4,15 +4,10 @@ socket.on('connect', function() {
   console.log('Connected to server');
 });
 
-socket.emit("createMessage", {
-  from: "Yana",
-  text: "Hey"
+socket.on("newMessage", function(message) {
+  console.log("New message", message);
 });
 
 socket.on('disconnect', function() {
   console.log('Disconnected from server');
 });
-
-socket.on("newMessage", function(message) {
-  console.log("New message", message);
-})
--- server/app.js
+++ server/app.js
@@ -13,14 +13,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => { //socket here represents each connection to the client
   console.log('New user connected');
 
-  socket.emit("newMessage", {
-    from: "Yana",
-    text: "Hey, what is going on",
-    createAt: 123
-  });
-
-  socket.on("createMessage", (newMessage) => {
-    console.log("createMessage", newMessage);
+  socket.on("createMessage", (message) => {
+    console.log("createMessage", message);
+    io.emit("newMessage", {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
